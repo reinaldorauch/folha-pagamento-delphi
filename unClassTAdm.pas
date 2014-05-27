49ba9aeefe4f5fd19116caf2baafa504b5f4cfc1
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2014-05-27 00:45:16 -0300

    Implementado método abstrato para o cálculo do salário total

--- unClassTAdm.pas
+++ unClassTAdm.pas
@@ -2,6 +2,22 @@ unit unClassTAdm;
 
 interface
 
+uses
+  unClassTFunc;
+
+Type
+  TAdmin = class(TFunc)
+    public
+      function SalarioTotal: double;
+  end;
+
 implementation
+
+{ TAdmin }
+
+function TAdmin.SalarioTotal: double;
+begin
+  result := Salario;
+end;
 
 end.
