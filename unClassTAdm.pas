adb4725af75a5e0b9d33724ac2a7b62fa906d4ed
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2014-05-27 01:27:48 -0300

    Adicionado redeclarações dos métodos construtores

--- unClassTAdm.pas
+++ unClassTAdm.pas
@@ -9,11 +9,23 @@ Type
   TAdmin = class(TFunc)
     public
       function SalarioTotal: double;
+      constructor Create; overload; override;
+      constructor Create (cNome: String; cSal: Double); overload; override;
   end;
 
 implementation
 
 { TAdmin }
+
+constructor TAdmin.Create(cNome: String; cSal: Double);
+begin
+  inherited;
+end;
+
+constructor TAdmin.Create;
+begin
+  inherited;
+end;
 
 function TAdmin.SalarioTotal: double;
 begin
