2dad92d91e6041ce3b7008f408fe0156d083b503
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2014-05-27 00:33:54 -0300

    Adicionado classe TFunc

---
+++ unClassTFunc.pas
@@ -0,0 +1,56 @@
+unit unClassTFunc;
+
+interface
+
+uses
+  SysUtils;
+
+Type
+  EFuncError      = class(Exception);
+  ENameLength     = class(EFuncError);
+  ENegativeSalary = class(EFuncError);
+
+  TFunc = class
+  private
+    procedure SetNome(const Value: String);
+    procedure SetSalario(const Value: Double);
+  protected
+    fNome: String;
+    fSalario: Double;
+    procedure Create (cNome: String; cSal: Double);
+    function salarioTotal: double; abstract;
+  public
+    property Nome: String read fNome write SetNome;
+    property Salario: Double read fSalario write SetSalario;
+end;
+
+implementation
+
+
+
+{ TFunc }
+
+procedure TFunc.Create(cNome: String; cSal: Double);
+begin
+  Nome := cNome;
+end;
+
+procedure TFunc.SetNome(const Value: String);
+begin
+  if((Length(value) > 3) OR (Value <> fNome)) then
+    fNome := Value
+  else
+    if(Length(value) <= 3) then
+      raise ENameLength.Create('Nome "' + Value + '" inválido, é necessário mais de 3 caracteres');
+end;
+
+procedure TFunc.SetSalario(const Value: Double);
+begin
+  if((Value <> fSalario) OR (Value) > 0) then
+    fSalario := Value
+  else
+    if Value <= 0 then
+      raise ENegativeSalary.Create('Salário' + FloatToStr(Value) + ' é inválido, é negativo');
+end;
+
+end.
