998d1d520183258581232ecf62dbec87f2af8304
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2014-05-27 01:28:16 -0300

    Criada reimplementação dos métodos construtores

--- unClassTSeller.pas
+++ unClassTSeller.pas
@@ -15,11 +15,34 @@ Type
       fSells: LongWord;
     public
       property Sells: LongWord read fSells write SetSells;
+      function SalarioTotal: double;
+      constructor Create (cNome: String; cSal: Double; cSells: Integer); overload;
+      constructor Create; overload; override;
   end;
+
+const Percent: byte = 3;
 
 implementation
 
 { TSeller }
+
+constructor TSeller.Create;
+begin
+  inherited;
+  Sells := 0;
+end;
+
+constructor TSeller.Create(cNome: String; cSal: Double; cSells: Integer);
+begin
+  Nome    := cNome;
+  Sells   := cSells;
+  Salario := cSal;
+end;
+
+function TSeller.SalarioTotal: double;
+begin
+  result := Salario + Sells * Percent / 100;
+end;
 
 procedure TSeller.SetSells(const Value: LongWord);
 begin
