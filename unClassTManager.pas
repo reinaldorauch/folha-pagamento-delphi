ba20f79239ce670e21af2e667351057d416410d2
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2014-05-27 02:02:50 -0300

    Criado setter do Bônus

--- unClassTManager.pas
+++ unClassTManager.pas
@@ -5,10 +5,30 @@ interface
 uses
   unClassTFunc;
 
-Type TManager = class(TFunc)
+Type
+  EManagerError = class(EFuncError);
+  ENegativeBonus = class(EManagerError);
 
-end;
+  TManager = class(TFunc)
+    private
+      procedure SetBonus(const Value: Double);
+    protected
+      fBonus: Double;
+    public
+      property Bonus: Double read fBonus write SetBonus;
+  end;
 
 implementation
+
+{ TManager }
+
+procedure TManager.SetBonus(const Value: Double);
+begin
+  if(Value <> fBonus) AND (Value >= 0) then
+    fBonus := Value
+  else
+    if Value < 0 then
+      raise ENegativeBonus.Create('O Bônus de ' + Value + ' é inválido, não pode ser negativo');
+end;
 
 end.
