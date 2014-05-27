8127dc7e4a35b046493f7c9007189513e8254bb3
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2014-05-27 02:05:40 -0300

    Implementado métodos construtores e SalárioTotal

--- unClassTManager.pas
+++ unClassTManager.pas
@@ -16,11 +16,32 @@ Type
       fBonus: Double;
     public
       property Bonus: Double read fBonus write SetBonus;
+      function SalarioTotal: double;
+      constructor Create (cNome: String; cSal, cBonus: Double); overload;
+      constructor Create; overload; override;
   end;
 
 implementation
 
 { TManager }
+
+constructor TManager.Create(cNome: String; cSal, cBonus: Double);
+begin
+  Salario := cSal;
+  Bonus   := cBonus;
+  Nome    := cNome;
+end;
+
+constructor TManager.Create;
+begin
+  inherited;
+  Bonus := 0;
+end;
+
+function TManager.SalarioTotal: double;
+begin
+  result := Salario + Bonus;
+end;
 
 procedure TManager.SetBonus(const Value: Double);
 begin
